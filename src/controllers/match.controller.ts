07dc4afee757c489640fc56a1891c3c5e0da6ebe
Rodolfo Talibs <rhodlib@gmail.com>
2020-11-04 13:02:34 -0300

    getByIdOrDate added and working, need better sintax

--- src/controllers/match.controller.ts
+++ src/controllers/match.controller.ts
@@ -10,3 +10,31 @@ export const getLastMatch = async (req: Request, res: Response) => {
         return res.status(404).json({ message: 'match not found' });
     }
 };
+
+//Get match by ID or Date.
+export const getByIdOrDate = async (req: Request, res: Response) => {
+    const { id, date } = req.body;
+    let match;
+
+    if (id) {
+        match = await Match.find({ apiId: id });
+    } else if (date) {
+        const finalDate = date
+            .substring(0, 8)
+            .concat(Number(date.substring(8)) + 1);
+        match = await Match.find({
+            $and: [
+                { date: { $gte: new Date(date) } },
+                { date: { $lt: new Date(finalDate) } },
+            ],
+        });
+    } else {
+        return res.status(400).json({ message: 'bad request' });
+    }
+
+    if (match) {
+        return res.status(200).json(match);
+    } else {
+        return res.status(404).json({ message: 'match not found' });
+    }
+};
--- src/routes/match.ts
+++ src/routes/match.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { TokenValidation } from '../middlewares/verifyToken';
-import { getLastMatch } from '../controllers/match.controller';
+import { getLastMatch, getByIdOrDate } from '../controllers/match.controller';
 
 const router = Router();
 
+router.get('/search', getByIdOrDate);
 router.get('/last', getLastMatch);
 
 export default router;
